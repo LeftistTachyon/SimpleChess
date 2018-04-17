f71cec52940e8b89f31d498e95a94bc10bc8055f
Gui Guy <jedandeganwang1999@gmail.com>
2018-04-17 15:22:02 -0500

    Minor changes

--- SimpleChessServer/src/simplechessserver/ClientCommunication.java
+++ SimpleChessServer/src/simplechessserver/ClientCommunication.java
@@ -1,7 +1,6 @@
 package simplechessserver;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
